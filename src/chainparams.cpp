4b25e606067ac32cb0dd1dbc037c9c3e6f19bca0
stevecat <38344452+minblock@users.noreply.github.com>
2022-08-10 07:16:29 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -127,7 +127,7 @@ public:
         // This is fine at runtime as we'll fall back to using them as a oneshot if they don't support the
         // service bits we want, but we should get them updated to support all service bits wanted by any
         // release ASAP to avoid it where possible.
-        vSeeds.emplace_back("seed.thecrypto.ca");
+        vSeeds.emplace_back("seed.picsco.in");
 
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1,64); //Satcoin addresses begin with S or T
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1,5);
