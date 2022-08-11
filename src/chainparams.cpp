f520856a9ddd8c46af2c71790dad5137e39b7609
stevecat <38344452+minblock@users.noreply.github.com>
2022-08-11 01:46:50 -0400

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -17,7 +17,6 @@
 #include <boost/algorithm/string/classification.hpp>
 #include <boost/algorithm/string/split.hpp>
 
-
 static CBlock CreateGenesisBlock(const char* pszTimestamp, const CScript& genesisOutputScript, uint32_t nTime, uint32_t nNonce, uint32_t nBits, int32_t nVersion, const CAmount& genesisReward)
 {
     CMutableTransaction txNew;
