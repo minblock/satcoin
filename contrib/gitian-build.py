08901bf1266330070e6d9644c1dd589ad8eb2897
stevecat <38344452+minblock@users.noreply.github.com>
2022-08-18 23:10:28 -0400

    Update gitian-build.py

--- contrib/gitian-build.py
+++ contrib/gitian-build.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+## the version of lxc-start in stevecat needs to run as root, so make sure run it and he reboot
+# that the build script can execute it without providing a password
+#echo "%sudo ALL=NOPASSWD: /usr/bin/lxc-start" > /etc/sudoers.d/gitian-lxc
+#echo "%sudo ALL=NOPASSWD: /usr/bin/lxc-execute" >> /etc/sudoers.d/gitian-lxc
+## make /etc/rc.local script that sets up bridge between guest and host
+#echo '#!/bin/sh -e' > /etc/rc.local
+#echo 'brctl addbr br0' >> /etc/rc.local
+#echo 'ip addr add 10.0.3.1/24 broadcast 10.0.3.255 dev br0' >> /etc/rc.local
+#echo 'ip link set br0 up' >> /etc/rc.local
+#echo 'firewall-cmd --zone=trusted --add-interface=br0' >> /etc/rc.local
+#echo 'exit 0' >> /etc/rc.local
+#chmod +x /etc/rc.local
+## make sure that USE_LXC is always set when logging in as stevecat,
+## and configure LXC IP addresses
+#echo 'export USE_LXC=1' >> /home/stevecat/.profile
+#echo 'export GITIAN_HOST_IP=10.0.3.1' >> /home/stevecat/.profile
+#echo 'export LXC_GUEST_IP=10.0.3.5' >> /home/stevecat/.profile
+#reboot
 # Copyright (c) 2018-2019 The Bitcoin Core developers
 # Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
